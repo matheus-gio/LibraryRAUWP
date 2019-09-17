2b16a944ffbd1e40cb0019acc90628dd000e14d1
matheus-gio <matheus.giovanni.lu@gmail.com>
2019-09-17 15:57:36 -0300

    d

--- docs/build/html/_sources/index.rst.txt
+++ docs/build/html/_sources/index.rst.txt
@@ -3,13 +3,14 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to SimplAR's documentation!
+Seja bem vindo a documentação do SimplAR!
 ===================================
 
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
 
+Biblioteca desenvolvida para realidade aumentada
 
 
 Indices and tables
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,6 @@
 # sys.path.insert(0, os.path.abspath('.'))
 import sphinx_rtd_theme
 
-
 # -- Project information -----------------------------------------------------
 
 project = 'SimplAR'
@@ -32,7 +31,8 @@ author = 'Matheus Lucena'
 extensions = [
 	"sphinx_rtd_theme",
 ]
-html_theme = "sphinx_rtd_theme"
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Add any paths that contain templates here, relative to this directory.
 #templates_path = ['_templates']
@@ -49,13 +49,6 @@ language = 'pt_BR'
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,

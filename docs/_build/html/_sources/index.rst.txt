4993cd154fc38e68cde9ee3b6e4eca3c7f8258d3
matheus-gio <matheus.giovanni.lu@gmail.com>
2019-09-17 15:04:57 -0300

    d

--- docs/_build/html/_sources/index.rst.txt
+++ docs/_build/html/_sources/index.rst.txt
@@ -1,5 +1,5 @@
 .. SimplAR documentation master file, created by
-   sphinx-quickstart on Tue Sep 17 14:39:01 2019.
+   sphinx-quickstart on Tue Sep 17 15:04:33 2019.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
--- docs/_build/html/_static/documentation_options.js
+++ docs/_build/html/_static/documentation_options.js
@@ -1,7 +1,7 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1',
-    LANGUAGE: 'None',
+    VERSION: '',
+    LANGUAGE: 'pt_BR',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
     HAS_SOURCE: true,
